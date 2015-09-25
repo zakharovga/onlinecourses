ee766b49e09d035cababdde4b19016ef1f4556dc
zakharovga <gosha-88@mail.ru>
2015-09-25 10:41:59 +0300

    test commit

--- src/main/java/com/onlinecourses/site/LoginController.java
+++ src/main/java/com/onlinecourses/site/LoginController.java
@@ -4,7 +4,6 @@ import com.onlinecourses.site.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
